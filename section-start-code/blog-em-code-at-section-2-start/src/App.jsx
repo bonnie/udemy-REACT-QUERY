804288e75cc1b2c30871335dba2615bd8805c99e
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-08-12 09:14:03 -0700

    Update syntax error QueryProvider --> QueryClientProvider

--- section-start-code/blog-em-code-at-section-2-start/src/App.jsx
+++ section-start-code/blog-em-code-at-section-2-start/src/App.jsx
@@ -1,4 +1,4 @@
-import { QueryClient, QueryProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 import { Posts } from "./Posts";
@@ -9,13 +9,13 @@ const queryClient = new QueryClient();
 function App() {
   return (
     // provide React Query client to App
-    <QueryProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <div className="App">
         <h1>Blog Posts</h1>
         <Posts />
         <ReactQueryDevtools />
       </div>
-    </QueryProvider>
+    </QueryClientProvider>
   );
 }
 
