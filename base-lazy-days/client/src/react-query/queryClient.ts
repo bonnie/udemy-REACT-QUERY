8c017211f425e7a1daa27ead4ad0597250c9643c
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-09-11 19:56:29 -0700

    Remove toast id from base lazy-days error handler

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -5,13 +5,12 @@
 
 // function queryErrorHandler(error: unknown): void {
 //   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const id = 'react-query-error';
 //   const title =
 //     error instanceof Error ? error.message : 'error connecting to server';
 
 //   // prevent duplicate toasts
 //   toast.closeAll();
-//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+//   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
 // }
 
 // to satisfy typescript until this file has uncommented contents
