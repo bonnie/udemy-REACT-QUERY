2b2ebc3cc61337b8803ffae13d7d6fb1c14691d8
Iago Medeiros <iagomedeiros.dev@gmail.com>
2023-04-30 13:11:50 -0300

    feat: :sparkles: Comments are now rendering

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,19 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts';
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
