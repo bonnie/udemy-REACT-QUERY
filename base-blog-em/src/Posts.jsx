ad06714d253f3d4b5268e26af48dd0449c2311d2
Iago Medeiros <iagomedeiros.dev@gmail.com>
2023-05-20 16:30:41 -0300

    feat: :zap: added pagination to page

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,56 +1,67 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { PostDetail } from './PostDetail';
-import { useQuery } from '@tanstack/react-query';
+import { PostDetail } from "./PostDetail";
+import { useQuery } from "@tanstack/react-query";
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
-  );
-  return response.json();
+async function fetchPosts(page) {
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=${maxPostPage}&_page=${page}`,
+	);
+	return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts);
+	const { data, isError, error, isLoading } = useQuery(["posts", currentPage], () =>
+		fetchPosts(currentPage),
+	);
 
-  return (
-    <>
-      <ul>
-        {isError && (
-          <>
-            <h1>DEU RUIM</h1>
-            <h2>{error.toString()}</h2>
-          </>
-        )}
-        {!isLoading &&
-          data?.map((post) => (
-            <li
-              key={post.id}
-              className="post-title"
-              onClick={() => setSelectedPost(post)}
-            >
-              {post.title}
-            </li>
-          ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {/* {selectedPost && selectedPost.id} */}
-      {selectedPost && <PostDetail post={selectedPost} />}
-
-    </>
-  );
+	return (
+		<>
+			<ul>
+				{isError && (
+					<>
+						<h1>DEU RUIM</h1>
+						<h2>{error.toString()}</h2>
+					</>
+				)}
+				{!isLoading &&
+					data?.map((post) => (
+						<li
+							key={post.id}
+							className="post-title"
+							onClick={() => setSelectedPost(post)}
+						>
+							{post.title}
+						</li>
+					))}
+			</ul>
+			<div className="pages">
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => {
+						setCurrentPage((prevState) => prevState - 1);
+					}}
+				>
+					Previous page
+				</button>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => {
+						setCurrentPage((prevState) => prevState + 1);
+					}}
+				>
+					Next page
+				</button>
+			</div>
+			<hr />
+			{/* {selectedPost && selectedPost.id} */}
+			{selectedPost && <PostDetail post={selectedPost} />}
+		</>
+	);
 }
