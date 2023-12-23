e2529a3702e8a7f6d3be2d1d658b0b92c94027e4
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-12-23 10:21:23 -0800

    More updates to base-blog-em

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,25 +1,5 @@
-async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
-}
-
-async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
-}
-
-async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
-}
+import { fetchComments } from "./api";
+import "./PostDetail.css";
 
 export function PostDetail({ post }) {
   // replace with useQuery
