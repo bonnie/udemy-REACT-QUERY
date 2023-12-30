986a1fb3e0ee379bb4fa799a928dc7e2c9cd2498
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-12-30 14:38:14 -0800

    Update app import in main.tsx

--- base-lazy-days/client/src/main.tsx
+++ base-lazy-days/client/src/main.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { App } from "components/app/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { theme } from "./theme";
+
+import { App } from "@/components/app/App";
+
 const root = document.getElementById("root");
 if (root) {
   ReactDOM.createRoot(root).render(
