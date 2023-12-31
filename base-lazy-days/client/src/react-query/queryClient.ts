4089d550d46b610fdf3dc45382c0db0169411c55
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-12-30 17:13:35 -0800

    More queryClient start code tweaks

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,20 +1,21 @@
-// import { toast } from "@/components/app/toast";
+import { QueryClient } from "@tanstack/react-query";
 
-// function errorHandler() {
-//     // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//     // one message per page load, not one message per query
-//     // the user doesn't care that there were three failed queries on the staff page
-//     //    (staff, treatments, user)
-//     const id = "react-query-toast";
+import { toast } from "@/components/app/toast";
 
-//     if (!toast.isActive(id)) {
-//       const action = "fetch";
-//       const title = `could not ${action} data: ${
-//         errorMsg ?? "error connecting to server"
-//       }`;
-//       toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//     }
+// function errorHandler(errorMsg: string) {
+//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+//   // one message per page load, not one message per query
+//   // the user doesn't care that there were three failed queries on the staff page
+//   //    (staff, treatments, user)
+//   const id = "react-query-toast";
+
+//   if (!toast.isActive(id)) {
+//     const action = "fetch";
+//     const title = `could not ${action} data: ${
+//       errorMsg ?? "error connecting to server"
+//     }`;
+//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
 //   }
+// }
 
-// to satisfy typescript until this file has uncommented contents
-export {};
+export const queryClient = new QueryClient();
