ee50943873450919d97dfcdf56ef2b3d8314cbba
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-01-03 15:59:28 -0800

    Remove unnecessary imports and mock

--- base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
@@ -2,15 +2,8 @@ import { screen } from "@testing-library/react";
 
 // import { http, HttpResponse } from "msw";
 // import { server } from '@/mocks/server';
-// import { defaultQueryClientOptions } from '@/react-query/queryClient';
-// import { renderWithClient } from '@/test-utils';
+// import { renderWithQueryClient } from '@/test-utils';
 import { Calendar } from "../Calendar";
-
-// mocking useUser to mimic a logged-in user
-// jest.mock('../../user/hooks/useUser', () => ({
-//   __esModule: true,
-//   useUser: () => ({ user: mockUser }),
-// }));
 
 test("Reserve appointment error", () => {
   // (re)set handler to return a 500 error for appointments
