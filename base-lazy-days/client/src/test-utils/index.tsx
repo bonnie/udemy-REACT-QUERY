256bcf03cafffa8a1ccb63adf30e90c15609bc03
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-01-03 15:59:43 -0800

    Start test utils out with chakra and react router providers

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,5 +1,9 @@
-import { render } from "@testing-library/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { render as RtlRender } from "@testing-library/react";
+import { ReactElement } from "react";
+import { MemoryRouter } from "react-router-dom";
 
+// ** FOR TESTING HOOKS ** //
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = () => {
 //   const queryClient = generateQueryClient();
@@ -7,3 +11,18 @@ import { render } from "@testing-library/react";
 //     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 //   );
 // };
+
+function customRender(ui: ReactElement) {
+  return RtlRender(
+    <ChakraProvider>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </ChakraProvider>
+  );
+}
+
+// re-export everything
+// eslint-disable-next-line react-refresh/only-export-components
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
