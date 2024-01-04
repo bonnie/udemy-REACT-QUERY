978d154bde571546ca31538961426cba2e6e50c8
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-01-04 10:08:26 -0800

    Update base test files

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -1,32 +1,29 @@
 import { Calendar } from "../Calendar";
 
-// import { fireEvent, screen, waitForElementToBeRemoved } from "@/test-utils";
+// import {
+//   fireEvent,
+//   render,
+//   screen,
+//   waitForElementToBeRemoved,
+// } from "@/test-utils";
 
 test("Reserve appointment", async () => {
-  // TODO: render the calendar
+  // // TODO: render the calendar
   //
-  // find all the appointments
-  //   const appointments = await screen.findAllByRole("button", {
-  //     name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
-  //   });
+  // // find all the appointments
+  // const appointments = await screen.findAllByRole("button", {
+  //   name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
+  // });
   //
-  //   // click on the first one to reserve
-  //   fireEvent.click(appointments[0]);
+  // // click on the first one to reserve
+  // fireEvent.click(appointments[0]);
   //
-  //   // check for the toast alert
-  //   const alertToast = await screen.findByRole("status");
-  //   expect(alertToast).toHaveTextContent("reserve");
+  // // check for the toast alert
+  // const alertToast = await screen.findByRole("status");
+  // expect(alertToast).toHaveTextContent("reserve");
   //
-  //   // close alert to keep state clean and wait for it to disappear
-  //   const alertCloseButton = screen.getByRole("button", { name: "Close" });
-  //   fireEvent.click(alertCloseButton);
-  //   await waitForElementToBeRemoved(alertToast);
-});
-
-test("Cancel appointment", async () => {
-  // TODO: your test here
-  //
-  // const cancelButtons = await screen.findAllByRole('button', {
-  //   name: /cancel appointment/i,
-  //  });
+  // // close alert to keep state clean and wait for it to disappear
+  // const alertCloseButton = screen.getByRole("button", { name: "Close" });
+  // fireEvent.click(alertCloseButton);
+  // await waitForElementToBeRemoved(alertToast);
 });
--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,5 +1,7 @@
 import { AllStaff } from "../AllStaff";
 
+// import { render, screen } from "@/test-utils";
+
 test("renders response from query", () => {
   // write test here
 });
